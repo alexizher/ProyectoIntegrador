7886aa9a09fbad5628464a36edff57d8062be924
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-02-14 09:12:44 -0500

    Se crea método para listar heroes y generar el log de info en caso no encontrarlo por id

--- tour-hero-api/src/main/java/co/udea/hero/api/service/HeroService.java
+++ tour-hero-api/src/main/java/co/udea/hero/api/service/HeroService.java
@@ -1,19 +1,40 @@
 package co.udea.hero.api.service;
 
 import co.udea.hero.api.model.Hero;
+import co.udea.hero.api.repository.HeroRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sun.misc.resources.Messages;
 
+import java.util.List;
 import java.util.Optional;
 
-@service
+@Service
+@Transactional
 public class HeroService {
-    private HeroRepository heroRepository;
 
-    public heroService(HeroRepository heroRepository){
+    private final Logger log = LoggerFactory.getLogger(HeroService.class);
+    private HeroRepository heroRepository;
+    private Messages messages;
+
+    public HeroService(HeroRepository heroRepository){
         this.heroRepository = heroRepository;
+        this.messages = messages;
     }
 
     public Hero getHero(Integer id){
         Optional<Hero> optionalHero = heroRepository.findById(id);
+        if(!optionalHero.isPresent()){
+            log.info("No se encuentra un heroe con ID: "+id);
+            throw new BusinessException(messages.get("exception.data_not_found.hero"));
+        }
         return optionalHero.get();
+    }
+
+    public List<Hero> getHeroes(){
+        List<Hero> heroesList = heroRepository.findAll();
+        return heroesList;
     }
 }
