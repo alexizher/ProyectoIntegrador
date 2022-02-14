b5d62446c45e55955f49a6f9ff51ed1bc4c02f59
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-02-14 06:55:36 -0500

    Se crea la clase Hero

---
+++ tour-hero-api/src/main/java/co/udea/hero/api/model/Hero.java
@@ -0,0 +1,37 @@
+package co.udea.hero.api.model;
+
+import com.sun.javafx.beans.IDProperty;
+
+@Entity
+@Table(name = "heroes")
+public class Hero {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    @column(name = "id")
+    private Integer id;
+
+    @Column (name = "name")
+    private String name;
+
+    public Hero(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
