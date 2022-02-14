0cd62bc0bc757f0b029e159a80de271228670694
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-02-14 09:14:26 -0500

    Se crean métodos y sus respectivas @notaciones para obtener heroe por ID y para obtener todos los heroes en Lista

--- tour-hero-api/src/main/java/co/udea/hero/api/controller/HeroController.java
+++ tour-hero-api/src/main/java/co/udea/hero/api/controller/HeroController.java
@@ -1,7 +1,19 @@
 package co.udea.hero.api.controller;
 
+import co.udea.hero.api.model.Hero;
+import co.udea.hero.api.service.HeroService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/heroes")
@@ -10,6 +22,31 @@ public class HeroController {
 
     public HeroController(HeroService heroService){
         this.heroService = heroService;
+    }
+
+    /**
+     * Busca un heroe creado en la DB po su ID
+     */
+    @GetMapping("{id}")
+    @ApiOperation(value = " Buscar un heroe por su id",  response = Hero.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Hero encontrado existosamente"),
+            @ApiResponse(code = 400, message = "La petición es invalida"),
+            @ApiResponse(code = 500, message = "Error interno al procesar la respuesta")
+    })
+    public ResponseEntity<Hero> getHero(@PathVariable Integer id){
+        log.info("Rest request buscar heroe por ID: "+ id);
+        return ResponseEntity.ok(heroService.getHero(id));
+    }
+
+    /**
+     * lista todos los heroes creados en la DB
+     */
+    @GetMapping("")
+    @ApiOperation(value = "Buscar todos los Heroes", response = List.class)
+    public ResponseEntity<List<Hero>> getHeroes(){
+        log.info("Rest request Buscar Heroes");
+        return ResponseEntity.ok(heroService.getHeroes());
     }
 
 }
