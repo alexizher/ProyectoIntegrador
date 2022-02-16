663977ea092dcfafff711cf9ee3244d3574d2115
Alexis Herrera <jalexis.herrera@udea.edu.co>
2022-02-15 21:45:49 -0500

    Se crea la clase Messages

---
+++ tour-hero-api/src/main/java/co/udea/hero/api/util/Messages.java
@@ -0,0 +1,34 @@
+package co.udea.hero.api.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Locale;
+
+@Component
+public class Messages {
+
+    @Autowired
+    private MessageSource messageSource;
+
+    private MessageSourceAccessor accessor;
+
+    @PostConstruct
+    private void init(){
+        accessor = new MessageSourceAccessor(messageSource, Locale.getDefault());
+    }
+
+    public String get(String code){
+        try{
+            return accessor.getMessage(code);
+        }
+        catch (NoSuchMessageException exception){
+            return "No hay mensaje disponible para mostrar.";
+        }
+
+    }
+}
